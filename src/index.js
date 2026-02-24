07733ff56d812cad0dcdf53b82a01f2adf328a4f
Ramires <ramiresdamazio@hotmail.com>
2026-02-23 21:47:17 -0300

    arrumei os if pra nao precisar cair no servidor pra validar

--- src/index.js
+++ src/index.js
@@ -23,9 +23,19 @@ try {
 app.post('/usuarios', async (req, res) => {
     try {
         const { nome, email, password } = req.body
+
+        if (!nome) throw new Error('Favor inserir seu nome')
+        if (!email) throw new Error('Favor inserir seu email')
+        if (!password) throw new Error('Favor inserir sua senha')
+
+        const userAlreadyExists = await User.findOne({ where: { email } })
+
+        if (userAlreadyExists) throw new Error('Já existe um usuário com esse email')
+
         const novoUsuario = await User.create({
             nome, email, password
         })
+
         res.status(201).json(novoUsuario)
     } catch (error) {
         res.status(400).json({ erro: 'não foi possivel criar o aluno', detalhes: error.message })
@@ -40,4 +50,14 @@ app.get('/usuarios', async (req, res) => {
         res.status(500).json({ erro: 'Falha ao buscar usuários', detalhes: error.message })
     }
 })
+
+app.put('/usuarios/:id', async (req, res) => {
+    const idDoAluno = req.params.id
+    const novosDados = req.body
+
+    const resultado = await User.update(novosDados, {
+        where: { id: idDoAluno }
+    })
+})
+
 app.listen(3000, () => console.log('servidor na porta http://localhost:3000'))
