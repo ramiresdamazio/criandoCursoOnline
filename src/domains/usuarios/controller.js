cda9f0ef470598dff10d5607d5dc44d056f0050e
ramires <ramiresdamazio@hotmail.com>
2026-03-05 17:55:17 -0300

    varios ajustes para aplicação não quebrar ou ter bugs, em usuario

--- src/domains/usuarios/controller.js
+++ src/domains/usuarios/controller.js
@@ -1,31 +1,40 @@
 import { User } from '../../models/models.js'
 import bcrypt from 'bcrypt'
+
 export class UsuariosController {
     async criarUsuario(req, res) {
         try {
             const { nome, email, password } = req.body
 
+            if (!nome || nome.trim() === '') return res.status(400).json({ erro: 'O nome precisa ser preenchido' })
+            if (!email || email.trim() === '') return res.status(400).json({ erro: 'O email precisa ser preenchido' })
+            if (!password) return res.status(400).json({ erro: 'O password precisa ser preenchido' })
+
             const userAlreadyExists = await User.findOne({ where: { email } })
 
-            if (userAlreadyExists) throw new Error('Já existe um usuário com esse email')
+            if (userAlreadyExists) return res.status(409).json({ erro: 'O usuário ja existe' })
 
             const senhaHash = await bcrypt.hash(password, 10)
 
             const novoUsuario = await User.create({
                 nome, email, password: senhaHash
             })
-            //olhar depois aqui embaixo
+
             const { password: _, ...usuarioSemSenha } = novoUsuario.toJSON()
             res.status(201).json(usuarioSemSenha)
-            res.status(201).json(novoUsuario)
+
         } catch (error) {
-            res.status(400).json({ erro: 'não foi possivel criar o aluno', detalhes: error.message })
+            console.log(error.message)
+            res.status(500).json({ erro: 'não foi possivel criar o aluno' })
         }
     }
 
     async listarUsuarios(req, res) {
         try {
-            const alunos = await User.findAll()
+            const alunos = await User.findAll({
+                attributes: { exclude: ['password'] }
+            })
+
             res.status(200).json(alunos)
         } catch (error) {
             res.status(500).json({ erro: 'Falha ao buscar usuários', detalhes: error.message })
@@ -38,8 +47,9 @@ export class UsuariosController {
             const alunoEncontrado = await User.findByPk(idDoAluno)
             if (!alunoEncontrado) throw new Error('Aluno não existe')
             res.status(200).json(alunoEncontrado)
+
         } catch (error) {
-            res.status(404).json({ erro: 'falha ao buscar usuario', detalhes: error.message })
+            res.status(409).json({ erro: 'falha ao buscar usuario', detalhes: error.message })
         }
     }
 
